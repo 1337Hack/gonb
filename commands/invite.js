f4175ee575400f49939fa2b9c53be59860b9395a
1337Hack <39503285+1337Hack@users.noreply.github.com>
2020-06-23 13:41:16 -0400

    Removed SlurDB API Call - Slur API would fail frequently so storing slurs locally as a quick fix - Added invite command to allow users to invite the bot to their server

---
+++ commands/invite.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+        name: 'invite',
+        description: 'A link to invite the bot to your discord server',
+        cooldown: 5,
+        execute(message, args) {
+                const embed = new Discord.RichEmbed()
+                .setColor('#00ff00')
+                .setTitle('Invite Gonb')
+		.setURL('https://discordapp.com/api/oauth2/authorize?client_id=678037877061582909&permissions=391232&scope=bot')
+                .setDescription('Invite Gonb bot to your own discord server.');
+                message.channel.send(embed);
+        },
+};
--- util/slurs.js
+++ util/slurs.js
@@ -1,15 +1,17 @@
 const fetch = require('node-fetch');
+const fs = require('fs');
 const slurURL = 'http://rsdb.org/api/json/all';
 let slurs = '';
 
 if(slurs == '') {
-	fetch(slurURL)
-	.then(res => res.json())
-	.then(json => {
-		slurs = json;
-		exports.data = slurs;
-	}).catch(err => console.log(err));  
+	slurs = JSON.parse(fs.readFileSync('util/slurs.json', 'utf8'));
+	exports.data = slurs;
+	//fetch('./slurs.json').catch(err => console.log(err))
+	//.then(res => res.json())
+	//.then(json => {
+	//	slurs = json;
+	//	exports.data = slurs;
+	//}).catch(err => console.log(err));  
 } else {
-	exports.data == slurs;
-}
-					
+	exports.data = slurs;
+}	
